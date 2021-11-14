c260dcc482cc00c1721f058922aa1a50f5f8e75e
Joe Ivans <contact@joeivans.dev>
2021-11-14 15:14:20 -0800

    Add remove method to Set
    - Adds tests
    - Adds implementation

--- sets/Set.js
+++ sets/Set.js
@@ -3,6 +3,20 @@
 class Set {
   set = {};
   size = 0;
+
+  add(value) {
+    if (!this.set.hasOwnProperty(value)) {
+      this.set[value] = value;
+      this.size++;
+    }
+  }
+
+  remove(value) {
+    if (this.set.hasOwnProperty(value)) {
+      delete this.set[value];
+      this.size--;
+    }
+  }
 }
 
 module.exports = Set;
--- sets/__tests__/set.test.js
+++ sets/__tests__/set.test.js
@@ -3,7 +3,47 @@
 const Set = require('../Set');
 
 describe('Set', function () {
-  it('should ...', function () {
+  it('should start empty', function () {
+    expect(set.size).toEqual(0);
+  });
+
+  it('should only add elements which are not already in the set', function () {
     const set = new Set();
+
+    // add element not in set
+    set.add('hello');
+    expect(set.size).toEqual(1);
+
+    // re-add element to set
+    set.add('hello');
+    expect(set.size).toEqual(1);
+
+    // add another element not in set
+    set.add('world');
+    expect(set.size).toEqual(2);
+  });
+
+  it('should only remove elements which are in the set', function () {
+    const set = new Set();
+
+    // adds one element
+    set.add('hello');
+    expect(set.size).toEqual(1);
+
+    // removes non-existent element
+    set.remove('world');
+    expect(set.size).toEqual(1);
+
+    // removes existent element
+    set.remove('hello');
+    expect(set.size).toEqual(0);
+  });
+
+  it('should nop on remove from empty set', function () {
+    const set = new Set();
+
+    // removes from empty set
+    set.remove('world');
+    expect(set.size).toEqual(0);
   });
 });
