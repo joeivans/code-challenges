f9dd783f1128c80abfe0293be7dfcedc8d353f38
Joe Ivans <joeivansdev@gmail.com>
2023-06-14 23:06:38 -0700

    Working on sliding window solution.

--- js/algo-challenges/number-of-arithmetic-triplets/__tests__/number-of-arithmetic-triplets.tests.js
+++ js/algo-challenges/number-of-arithmetic-triplets/__tests__/number-of-arithmetic-triplets.tests.js
@@ -1,6 +1,7 @@
 const {arithmeticTriplets} = require('../index');
 
 describe('Number of Arithmetic Triplets', function () {
+
   it(`([0, 1, 4, 6, 7, 10], 3) => 2`, function () {
     const result = arithmeticTriplets([0, 1, 4, 6, 7, 10], 3);
     expect(result).toEqual(2);
@@ -10,4 +11,15 @@ describe('Number of Arithmetic Triplets', function () {
     const result = arithmeticTriplets([4, 5, 6, 7, 8, 9], 2);
     expect(result).toEqual(2);
   });
+
+  it(`([6,14,15,26,31,36,38,41,42,45], 5) => 2`, function () {
+    const result = arithmeticTriplets([6, 14, 15, 26, 31, 36, 38, 41, 42, 45], 5);
+    expect(result).toEqual(2);
+  });
+
+  it(`([2,10,14,19,20,24,30,34,45,48], 10) => 2`, function () {
+    const result = arithmeticTriplets([2, 10, 14, 19, 20, 24, 30, 34, 45, 48], 10);
+    expect(result).toEqual(2);
+  });
+
 });
--- js/algo-challenges/number-of-arithmetic-triplets/index.js
+++ js/algo-challenges/number-of-arithmetic-triplets/index.js
@@ -4,20 +4,71 @@
  * @return {number}
  */
 const arithmeticTriplets = function (nums, diff) {
-// O(n) Time | O(n) Space
+// O(n) Time | O(1) Space
 //  n = nums.length
 //  Algorithm scales linearly with input array size.
+//
+// NOTES:
+//  This one's funky. Because the input is strictly increasing, we can use a
+//  sliding window with as many pointers as we want as long as they are also
+//  strictly increasing.
+//
+//  This means that for i, j, k:
+//    - They will each never exceed n.
+//    - They will each never be assigned the same value more than once.
+//
+//  Therefore, the work done in each nested loop is so amortized as constant.
+//  The maximum number of operations in the entire algorithm will never exceed
+//  3n because there are 3 variables sliding across a range of n, which
+//  summarizes to O(n) Time.
 
-  let count = 0;
-  const set = new Set(nums);
+  const n = nums.length;
+  const minN = 3;
+  const maxN = 300;
 
-  for (const num of nums) {
-    if (set.has(num - diff) && set.has(num + diff)) {
-      count++;
+  let answer = 0;
+
+  let i = 0;
+  let j = 1;
+  let k = 2;
+
+  // entire window
+  while (i < n - 2) {
+    // left side
+    while (i < j && nums[j] - nums[i] !== diff) {
+      // grow left side gte constraint
+      while (j < n - 1 && nums[j] - nums[i] < diff) {
+        j++;
+      }
+      // shrink left side to equal constraint
+      while (i < j && nums[j] - nums[i] > diff) {
+        i++;
+      }
     }
+
+    k = j + 1;
+
+    // right side
+    while (k < n && j < k && nums[k] - nums[j] !== diff) {
+      // grow right side gte constraint
+      while (k < n && nums[k] - nums[j] < diff) {
+        k++;
+      }
+      // shrink right side to equal constraint
+      while (j < k && nums[k] - nums[j] > diff) {
+        j++;
+      }
+    }
+
+    // update answer
+    if (nums[j] - nums[i] === diff && nums[k] - nums[j] === diff) {
+      answer++;
+    }
+
+    i++;
   }
 
-  return count;
+  return answer;
 };
 
 module.exports = {
