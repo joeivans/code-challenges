f8863a088f5e81a9139ece31d8a44fc699cb6d84
Joe Ivans <joeivansdev@gmail.com>
2023-07-01 21:16:51 -0700

    Challenge: Get Equal Substrings Within Budget
    Summary:
    	- Added second attempt solution.

--- js/algo-challenges/get-equal-substrings-within-budget/index.js
+++ js/algo-challenges/get-equal-substrings-within-budget/index.js
@@ -6,31 +6,38 @@
  */
 const equalSubstring = function (s, t, maxCost) {
 // O(n) Time | O(1) Space
-//  n = s.length = t.length
-//  Algorithm scales linearly with input string size.
+//  n = s.length = t.length;
+//  Algorithm scales linearly with input string sizes.
+
+  const n = s.length;
 
   let maxLength = 0;
-  let cost = 0;
 
-  for (let l = 0, r = 0; r < s.length; r++) {
-    cost += getAsciiAbsDiff(s[r], t[r]);
+  for (let l = 0, r = 0, currCost = 0; r < n; r++) {
+    // grow window
+    //
+    currCost += getCost(s[r], t[r]);
 
-    while (cost > maxCost && l <= r) {
-      cost -= getAsciiAbsDiff(s[l], t[l]);
+    // shrink window
+    //
+    while (l <= r && currCost > maxCost) {
+      currCost -= getCost(s[l], t[l]);
+
       l++;
     }
 
+    // update answer
+    //
     if (r - l + 1 > maxLength) {
       maxLength = r - l + 1;
     }
   }
 
   return maxLength;
+};
 
-  function getAsciiAbsDiff(a, b) {
-    return Math.abs(
-      a.charCodeAt(0) - b.charCodeAt(0));
-  }
+const getCost = function (a, b) {
+  return Math.abs(a.charCodeAt(0) - b.charCodeAt(0));
 };
 
 module.exports = {
