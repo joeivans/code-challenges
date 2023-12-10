1ef45f25559ab2dd3f765eeba6eee7ad538393d5
Joe Ivans <joeivansdev@gmail.com>
2023-12-09 16:35:41 -0800

    Challenge: Middle of the Linked List
    Summary:
    	- Added challenge whiteboard image file.
    	- Added challenge README file.
    	- Added challenge tests.
    	- Added challenge solution.
    	- Updated repo README, added challenge link.

---
+++ js/algo-challenges/middle-of-the-linked-list/index.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const middleNode = function (head) {
+// O(n) Time | O(1) Space | n = count of nodes in head
+
+  let fast = head;
+  let slow = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  return slow;
+};
+
+module.exports = {
+  middleNode
+};
